24e0e17abdddfec8e2888e7acb2081b73f3e489f
Tommy <39845906+Tommy-pixels@users.noreply.github.com>
2023-10-10 16:12:39 +0800

    20231010 新增脚本  股票日数据录入

---
+++ script/add_all_stock_daily_data.py
@@ -0,0 +1,70 @@
+"""
+更新添加所有股票的日数据
+"""
+import time
+from single_stock.data_fetcher import Stock
+from database.base import Tb_Stock_Info, Tb_Stock_Daily_Data
+
+
+def run():
+    # 1 创建实例
+    stock_info_inst = Tb_Stock_Info()
+    stock_daily_data_inst = Tb_Stock_Daily_Data()
+    stock_inst = Stock()
+    cursor = stock_info_inst.get_cursor()
+    # 2 获取要提取日数据的股票列表
+    stock_lis = stock_info_inst.filter_stock_info(cursor=cursor, belong_lis=['沪市A股', '深市A股'])
+    # 3 获取股票日数据并录入数据库
+    for stock in stock_lis:
+        stock_code = stock[0]
+        if(stock_code.startswith('00')):
+            # 深市 股市成立时间19910703
+            data = stock_inst.single_stock_data(
+                stock=stock,
+                period='daily',
+                start_date='19910703'
+            )
+        else:
+            # 沪市 股市成立时间19901219
+            data = stock_inst.single_stock_data(
+                stock=stock,
+                period='daily',
+                start_date='19901219'
+            )
+        total = len(data)
+        success_lis = []
+        for i in range(total):
+            _ = data.iloc[i]
+            date = _['日期']
+            item_dic = {
+                'opening_price': _["开盘"],
+                'closing_price': _["收盘"],
+                'the_highest_price': _["最高"],
+                'the_lowest_price': _["最低"],
+                'trading_volume': _["成交量"],
+                'business_volume': _["成交额"],
+                'amplitude': _["振幅"],
+                'advance_decline_amplitude': _["涨跌幅"],
+                'advance_decline_volume': _["涨跌额"],
+                'turnover_rate': _["换手率"],
+                'note': ''
+            }
+            res = stock_daily_data_inst.add_stock_info(
+                cursor=cursor,
+                stock_code=stock_code,
+                date=date,
+                **item_dic
+            )
+            if(res):
+                success_lis.append(item_dic)
+        if(len(success_lis) == total):
+            print('{} 日数据共 {} 条，录入 {} 条'.format(stock, total, len(success_lis)))
+        else:
+            print('Error: {} 日数据共 {} 条，录入 {} 条'.format(stock, total, len(success_lis)))
+            # 添加操作记录
+        time.sleep(5)
+
+
+if __name__ == '__main__':
+    print('录入股票日数据')
+    run()
--- utils/common.py
+++ utils/common.py
@@ -8,3 +8,17 @@ class Control_Time:
         # 是否是工作日
         return datetime.datetime.today().weekday() < 5
 
+    @classmethod
+    def between_n_days(cls, date, time_format='%Y-%m-%d', n_days:int=-30):
+        """判断指定日期是否在指定时间内
+        :param n_days 在指定天数内 >0表示在未来n天内     <0表示在过去n天内
+        """
+        delta_days = (datetime.datetime.strptime(date, time_format) - datetime.datetime.today()).days
+        if((delta_days>0 and delta_days<=n_days) or (delta_days<0 and delta_days>=n_days)):
+            return True
+        else:
+            return False
+
+
+if __name__ == '__main__':
+    print(Control_Time.between_n_days(date='2023-10-01', n_days=10))

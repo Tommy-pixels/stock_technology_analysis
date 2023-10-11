007bcbb3e2e4f847a57a2dcbce1df6a58fb5d971
Tommy <39845906+Tommy-pixels@users.noreply.github.com>
2023-10-11 09:56:35 +0800

    20231011 添加操作记录

--- script/add_all_stock_daily_data.py
+++ script/add_all_stock_daily_data.py
@@ -4,6 +4,8 @@
 import time
 from single_stock.data_fetcher import Stock
 from database.base import Tb_Stock_Info, Tb_Stock_Daily_Data
+from database.log import Tb_Log
+from utils.common import Control_Time
 
 
 def run():
@@ -12,10 +14,13 @@ def run():
     stock_daily_data_inst = Tb_Stock_Daily_Data()
     stock_inst = Stock()
     cursor = stock_info_inst.get_cursor()
+    action_name = '录入股票日数据'
+    operation_id = Tb_Log.generate_operation_id(name=action_name)
+
     # 2 获取要提取日数据的股票列表
     stock_lis = stock_info_inst.filter_stock_info(cursor=cursor, belong_lis=['沪市A股', '深市A股'])
     # 3 获取股票日数据并录入数据库
-    for stock in stock_lis:
+    for stock in stock_lis[143:]:
         stock_code = stock[0]
         if(stock_code.startswith('00')):
             # 深市 股市成立时间19910703
@@ -31,6 +36,21 @@ def run():
                 period='daily',
                 start_date='19901219'
             )
+        if(data is None or data.empty):
+            # 添加操作记录
+            action_status = '获取到日数据为空'
+            Tb_Log.create_log(
+                cursor=cursor,
+                operation_id=operation_id,
+                action='录入股票日数据',
+                action_detail=stock[0] + '-' + stock[1],
+                action_status=action_status,
+                create_time=Control_Time.get_cur_date('%Y-%m-%d %H:%M:%S'),
+                stock=stock,
+                excuting_file='add_all_stock_daily_data.py',
+                note=''
+            )
+            continue
         total = len(data)
         success_lis = []
         for i in range(total):
@@ -59,9 +79,23 @@ def run():
                 success_lis.append(item_dic)
         if(len(success_lis) == total):
             print('{} 日数据共 {} 条，录入 {} 条'.format(stock, total, len(success_lis)))
+            action_status = '录入数据完成，共{}条'.format(total)
         else:
             print('Error: {} 日数据共 {} 条，录入 {} 条'.format(stock, total, len(success_lis)))
-            # 添加操作记录
+            action_status = '录入数据不全'
+        # 添加操作记录
+        Tb_Log.create_log(
+            cursor=cursor,
+            operation_id=operation_id,
+            action='录入股票日数据',
+            action_detail=stock[0] + '-' + stock[1],
+            action_status=action_status,
+            create_time=Control_Time.get_cur_date('%Y-%m-%d %H:%M:%S'),
+            stock=stock,
+            start_end_date_section=[data.iloc[0]['日期'], data.iloc[len(data)-1]['日期']],    # 起止日期
+            excuting_file='add_all_stock_daily_data.py',
+            note=''
+        )
         time.sleep(5)
 
 
